8c7bdb088d576efba7b72f4050254120e3b29ff8
ammarku <100276343+ammarku@users.noreply.github.com>
2022-05-25 20:38:11 +0700

    Update rdp-chrome.sh

--- rdp-chrome.sh
+++ rdp-chrome.sh
@@ -5,9 +5,9 @@ Pin=123456
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb > /dev/null 2>&1
 dpkg --install chrome-remote-desktop_current_amd64.deb > /dev/null 2>&1
 apt install --assume-yes --fix-broken > /dev/null 2>&1
-echo "task 01"
+echo "task 01"$Pin
 export DEBIAN_FRONTEND=noninteractive
-echo "task 02"
+echo "task 02"$CRPCROME
 apt install --assume-yes xfce4 desktop-base xfce4-terminal > /dev/null 2>&1
 echo "task 03"
 bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session"> /etc/chrome-remote-desktop-session'
@@ -32,8 +32,8 @@ command = $CRPCROME --pin=$Pin > /dev/null 2>&1
 echo "task 12"
 su - user -c $command > /dev/null 2>&1
 echo "task 13"
-wget https://gitlab.com/mailzakode/ku/-/raw/main/tools/anticaptcha-plugin_v0.62.zip -O /home/admin/anticaptcha-plugin_v0.62.zip && cd /home/admin && unzip anticaptcha-plugin_v0.62.zip > /dev/null 2>&1
+wget https://gitlab.com/mailzakode/ku/-/raw/main/tools/anticaptcha-plugin_v0.62.zip -O /home/user/anticaptcha-plugin_v0.62.zip && cd /home/user && unzip anticaptcha-plugin_v0.62.zip > /dev/null 2>&1
 echo "task 10"
-wget https://gitlab.com/mailzakode/ku/-/raw/main/tools/ext.sh -O /home/admin/ext.sh && cd /home/admin && sudo sh ext.sh > /dev/null 2>&1
+wget https://gitlab.com/mailzakode/ku/-/raw/main/tools/ext.sh -O /home/admin/ext.sh && cd /home/user && sudo sh ext.sh > /dev/null 2>&1
 
 echo "task 10"service chrome-remote-desktop start > /dev/null 2>&1
