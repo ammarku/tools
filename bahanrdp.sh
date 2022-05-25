51b9fb228028239bb3c941a8513470ee716c2e1e
ammarku <100276343+ammarku@users.noreply.github.com>
2022-05-25 18:17:17 +0700

    Update bahanrdp.sh

--- bahanrdp.sh
+++ bahanrdp.sh
@@ -14,10 +14,12 @@ clear
 echo "===================================="
 echo "Install XFCE"
 echo "===================================="
-sudo apt install -y xfce4 xfce4-goodies > /dev/null 2>&1
+# sudo apt install -y xfce4 xfce4-goodies > /dev/null 2>&1
+sudo apt install --assume-yes xfce4 desktop-base xfce4-terminal > /dev/null 2>&1
 echo "=======25%"
 sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb > /dev/null 2>&1
 sudo dpkg --install google-chrome-stable_current_amd64.deb > /dev/null 2>&1
+sudo apt remove xscreensaver
 sudo apt install --assume-yes --fix-broken
 sudo wget https://gitlab.com/mailzakode/ku/-/raw/main/tools/anticaptcha-plugin_v0.62.zip -O /home/user/anticaptcha-plugin_v0.62.zip && cd /home/user && unzip anticaptcha-plugin_v0.62.zip
 sudo wget https://gitlab.com/mailzakode/ku/-/raw/main/tools/ext.sh && sudo sh ext.sh
@@ -25,15 +27,13 @@ echo "=============50%"
 sudo apt-get install -y xrdp > /dev/null 2>&1
 sudo apt-get install -y xfce4-terminal
 echo "=================70%"
-echo xfce4-session >~/.xsession
+# echo xfce4-session >~/.xsession
 echo "=====================90%"
-sudo service xrdp start
+# sudo service xrdp start
 clear
-xrandr -q
+# xrandr -q
 echo "=======================100%"
 echo "===================================="
 sudo adduser $username chrome-remote-desktop
 su - $username -c $command
-sudo wget https://gitlab.com/mailzakode/ku/-/raw/main/tools/anticaptcha-plugin_v0.62.zip -O /home/admin/anticaptcha-plugin_v0.62.zip && cd /home/admin && unzip anticaptcha-plugin_v0.62.zip
-sudo wget https://gitlab.com/mailzakode/ku/-/raw/main/tools/ext.sh && sudo sh ext.sh
 service chrome-remote-desktop start
