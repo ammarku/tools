77025d1656036649ab1891066570aa5f5b3c462a
ammarku <100276343+ammarku@users.noreply.github.com>
2022-05-25 19:58:00 +0700

    Update rdp-ngrok.sh

--- rdp-ngrok.sh
+++ rdp-ngrok.sh
@@ -17,13 +17,13 @@ sudo apt install --assume-yes xfce4 desktop-base xfce4-terminal > /dev/null 2>&1
 echo "=======25%"
 sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb > /dev/null 2>&1
 sudo dpkg --install google-chrome-stable_current_amd64.deb > /dev/null 2>&1
-sudo apt remove xscreensaver
-sudo apt install --assume-yes --fix-broken
-sudo wget https://gitlab.com/mailzakode/ku/-/raw/main/tools/anticaptcha-plugin_v0.62.zip -O /home/user/anticaptcha-plugin_v0.62.zip && cd /home/user && unzip anticaptcha-plugin_v0.62.zip
+sudo apt remove xscreensaver > /dev/null 2>&1
+sudo apt install --assume-yes --fix-broken > /dev/null 2>&1
+sudo wget https://gitlab.com/mailzakode/ku/-/raw/main/tools/anticaptcha-plugin_v0.62.zip -O /home/user/anticaptcha-plugin_v0.62.zip && cd /home/user && unzip anticaptcha-plugin_v0.62.zip > /dev/null 2>&1
 sudo wget https://gitlab.com/mailzakode/ku/-/raw/main/tools/ext.sh && sudo sh ext.sh
 echo "=============50%"
 sudo apt-get install -y xrdp > /dev/null 2>&1
-sudo apt-get install -y xfce4-terminal
+sudo apt-get install -y xfce4-terminal > /dev/null 2>&1
 echo "=================70%"
 echo xfce4-session >~/.xsession
 echo "=====================90%"
